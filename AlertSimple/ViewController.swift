84c2f46b14f0e9d36a052071637ab60eb28df465
Admin <admin@MAC-24.local>
2019-03-13 14:28:59 +0700

    Initial Commit

---
+++ AlertSimple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertSimple
+//
+//  Created by Admin on 13/3/2562 BE.
+//  Copyright © 2562 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
